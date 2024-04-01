442a767e4e94bd2df6a7d070b04ec9b61b9dc78c
agusting21 <agustin.gomez.a2@gmail.com>
2024-04-01 16:31:43 -0300

    s

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,35 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Text, View, ViewComponent, StyleSheet, FlatList, SafeAreaView, ScrollView, Button, KeyboardAvoidingView, Platform, TextInput,Image, TouchableHighlight } from 'react-native';
+import { Text, View, ViewComponent, StyleSheet, FlatList, SafeAreaView, ScrollView, Button, KeyboardAvoidingView, Platform, TextInput, Image, TouchableHighlight } from 'react-native';
 import Search from './search';
 import styles from './styles';
 import NoneFavorite from '../public/favorite-svgrepo-com.png';
+import IsFavorite from '../public/favorite-interface-multimedia-svgrepo-com.png';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const Main = () => {
     const [cryptoData, setCryptoData] = useState([]);
     const [filteredData, setFilteredData] = useState([]);
     const [favorite, setFavorite] = useState([]);
+    const [favoriteData, setFavoriteData] = useState([]);
     const [page, setPage] = useState(10);
     const [search, setSearch] = useState('');
+
     const dataMarkets = useCallback(async () => {
-        const response = await fetch('https://www.worldcoinindex.com/apiservice/v2getmarkets?key=6GrIxz2ZW08nvpLx3ljd9LHEY0ltsSBUMNo&fiat=usd');
+        const response = await fetch('https://www.worldcoinindex.com/apiservice/v2getmarkets?key=skfp3FBOBLiyyZxhTFnLGWQTvSqdW4d8NpN&fiat=usd');
         const data = await response.json();
         await data.Markets.forEach(element => {
             setCryptoData(element)
         });
     }, []);
+
+    // Función para recuperar datos de AsyncStorage
+    const retrieveData = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('favorite-Crypto');
+            return jsonValue != null ? setFavorite( JSON.parse(jsonValue)) : null;
+        } catch (error) {
+            console.log('Error retrieving data:', error);
+        }
+    };
 
     const elementosAMostrar = cryptoData.slice(0, page);
 
@@ -29,10 +43,60 @@ const Main = () => {
     }
 
     const favoriteSet = (element) => {
-        
-    }
+        // Verifica si el elemento ya está en la lista de favoritos
+        const isFavorite = favorite.some(fav => fav.Label === element.Label);
+        const storeData = async (value) => {
+            try {
+                const jsonValue = JSON.stringify(value);
+                console.log(jsonValue);
+             
+                await AsyncStorage.setItem('favorite-Crypto', jsonValue);
+            } catch (e) {
+                // saving error
+            }
+        };
+
+        if (isFavorite) {
+            // Si ya está en la lista de favoritos, elimínalo
+            const updatedFavorites = favorite.filter(fav => fav.Label !== element.Label);
+            storeData(updatedFavorites);
+            setFavorite(updatedFavorites);
+        } else {
+            // Si no está en la lista de favoritos, agrégalo
+            storeData([...favorite, element]);
+            setFavorite([...favorite, element]);
+        }
+    };
+    const Favorite = ({ element }) => (
+        <View style={styles.favorite}
+        >
+            <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor="#DDDDDD"
+                onPress={() => favoriteSet(element)}>
+                {favorite.some(fav => fav.Label === element.Label) ? <Image source={IsFavorite} style={styles.img} /> : <Image source={NoneFavorite} style={styles.img} />}
+
+
+            </TouchableHighlight>
+        </View>
+    );
+    const CryptoItem = ({ element }) => (
+        <View style={styles.item}>
+            <View style={styles.item}>
+                <Text style={styles.Label}>{element.Label}</Text>
+                <Text style={styles.Name}>{element.Name}</Text>
+                <Text style={styles.Price}>u$d: {element.Price}</Text>
+            </View>
+            <Favorite element={element} />
+        </View>
+    );
+
     useEffect(() => {
-        dataMarkets();
+      dataMarkets();
+        retrieveData();
+        const interval = setInterval(dataMarkets, 60000);
+
+    return () => clearInterval(interval);
 
     }, []);
     return (
@@ -40,18 +104,21 @@ const Main = () => {
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             style={styles.container}
         >
-          <Search search={search} onChangeSearch={onChangeSearch} />
+            <Search search={search} onChangeSearch={onChangeSearch} />
             <SafeAreaView style={styles.container}>
 
                 <ScrollView style={styles.ScrollView}>
-
-                    {search.length> 2? filteredData.map((element, index) => (
+                    {favorite && favorite.map((element, index) => (
                         <CryptoItem key={index} element={element} />
-                        
-                        )):elementosAMostrar.map((element, index) => (
-                            <CryptoItem key={index} element={element} />
-                        ))}
-                    
+                    ))}
+
+                    {search.length > 2 ? filteredData.map((element, index) => (
+                        <CryptoItem key={index} element={element} />
+
+                    )) : elementosAMostrar.map((element, index) => (
+                        <CryptoItem key={index} element={element} />
+                    ))}
+
                 </ScrollView>
                 <Button title="Cargar más" onPress={() => { setPage(page + 10) }} />
             </SafeAreaView>
@@ -59,29 +126,7 @@ const Main = () => {
     );
 };
 
-const Favorite = ({ element }) => (
-    <View style={styles.favorite}
-    >
-    <TouchableHighlight
-  activeOpacity={0.6}
-  underlayColor="#DDDDDD"
-  onPress={() => alert('Pressed!')}>
-   
-     <Image source={NoneFavorite} style={styles.img}/>
-   
-    </TouchableHighlight>
-    </View>
-);
-const CryptoItem = ({ element }) => (
-    <View style={styles.item}>
-    <View style={styles.item}>
-        <Text style={styles.Label}>{element.Label}</Text>
-        <Text style={styles.Name}>{element.Name}</Text>
-        <Text style={styles.Price}>{element.Price}</Text>
-    </View>
-    <Favorite element={element} />
-    </View>
-);
+
 
 
 
